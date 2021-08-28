29e15b8996dad889c9ff162876079534673589ff
ALEX-WS-LIN <amcunio@bigpond.net.au>
2021-08-28 18:46:06 +1000

    Progress on dashboard

--- syncshack-2021/src/App.js
+++ syncshack-2021/src/App.js
@@ -7,31 +7,31 @@ import Playground from './pages/Playground';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route
-          exact
-          path="/"
-          render={(props) => {
-            return <Login {...props} />;
-          }}
-        />
-        <Route
-          exact
-          path="/dashboard"
-          render={(props) => {
-            return <Dashboard {...props} />;
-          }}
-        />
-        <Route
-          exact
-          path="/playground"
-          render={(props) => {
-            return <Playground {...props} />;
-          }}
-        />
-      </Switch>
-    </Router>
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={(props) => {
+              return <Login {...props} />;
+            }}
+          />
+          <Route
+            exact
+            path="/dashboard"
+            render={(props) => {
+              return <Dashboard {...props} />;
+            }}
+          />
+          <Route
+            exact
+            path="/playground"
+            render={(props) => {
+              return <Playground {...props} />;
+            }}
+          />
+        </Switch>
+      </Router>
   );
 }
 
