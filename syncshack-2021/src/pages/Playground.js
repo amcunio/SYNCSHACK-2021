1107d54c63a2b1046f12e0855b99aea89b465803
Rachel Yu <z5316329@student.unsw.edu.au>
2021-08-28 17:15:17 +1000

    make dog different when walking diff direction

--- syncshack-2021/src/pages/Playground.js
+++ syncshack-2021/src/pages/Playground.js
@@ -7,13 +7,18 @@ import ChatBox from "../components/Chat/Chat";
 import anime from "animejs";
 // import initialise from '../components/Animation/animate'
 
-
+const standingL = "/static/dogStandingL.gif"
+const standingR = "/static/dogStandingR.gif"
+const runningL = "/static/dogRunningL.gif"
+const runningR = "/static/dogRunningR.gif"
+var prevPos = 400
 
 function Playground() {
   const [showGoals, setShowGoals] = React.useState(false);
   const [showChat, setShowChat] = React.useState("None");
   const [isWalk, setIsWalk] = React.useState(false);
-  const [back, setBack] = React.useState(1)
+  const [isRight, setIsRight] = React.useState(false)
+
   const toggleChat = () => {
     if (showChat === "None") {
       setShowChat("block")
@@ -25,9 +30,13 @@ function Playground() {
   const getRandomPos = (minX, maxX, minY, maxY) => {
     const x = Math.random() * (maxX - minX) + minX;
     const y = Math.random() * (maxY - minY) + minY;
-    // if (x < 0) {
-    //   setBack(-1)
-    // }
+
+    if (prevPos < x) {
+      setIsRight(true)
+    } else {
+      setIsRight(false)
+    }
+    prevPos = x
     return { x, y };
   };
 
@@ -73,7 +82,7 @@ function Playground() {
       <Drawer open={showGoals} onClose={() => setShowGoals(!showGoals)}><Goal /></Drawer>
       <div className={styles.chatToggle} onClick={toggleChat} style={{display: showChat==="None" ? "block" : "None"}}>Chat</div>
       <ChatBox open={showChat !== 'None'} setOpen={setShowChat} />
-      <img src={isWalk ? "/static/dogWalking.gif" : "/static/dogStanding.gif"} className={`${styles.dog} dog`}  onClick={handleJump}/>
+      <img src={isRight ? (isWalk ? runningR : standingR) : (isWalk ? runningL : standingL)} className={`${styles.dog} dog`}  onClick={handleJump} alt="pet"/>
     </div>
   );
 }
