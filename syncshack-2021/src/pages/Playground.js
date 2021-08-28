a483f27d6ae9e1ab16055d7360ef11108b20a881
Rachel Yu <z5316329@student.unsw.edu.au>
2021-08-28 12:45:31 +1000

    import dog gif

--- syncshack-2021/src/pages/Playground.js
+++ syncshack-2021/src/pages/Playground.js
@@ -4,8 +4,6 @@ import styles from "./Playground.module.css";
 import { Drawer } from "@material-ui/core";
 import Goal from "../components/Goal/Goal";
 import Chat from "../components/Chat/Chat";
-import bg from "./background.png"
-import dog from "./dog.gif"
 import initialise from '../components/Animation/animate'
 
 function Playground() {
@@ -25,7 +23,7 @@ function Playground() {
   }, [])
   return (
     <div className={styles.page}>
-      <img src={bg} className={styles.background} alt="background"/>
+      <img src="/static/background.png" className={styles.background} alt="background"/>
       <div
         className={styles.goalToggle}
         onClick={() => setShowGoals(!showGoals)}
@@ -35,7 +33,7 @@ function Playground() {
       <Drawer open={showGoals} onClose={() => setShowGoals(!showGoals)}><Goal /></Drawer>
       <div className={styles.chatToggle} onClick={toggleChat} style={{display: showChat==="None" ? "block" : "None"}}>Chat</div>
       <Chat open={showChat} setOpen={setShowChat} />
-      <img src={dog} className={`${styles.dog} dog`}/>
+      <img src="/static/dogStanding.gif" className={`${styles.dog} dog`}/>
     </div>
   );
 }
